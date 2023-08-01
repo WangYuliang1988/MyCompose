ef200cdb68e1b31f246c88fef0c74e2ac6634eaf
wangyuliang <langzi1206@msn.cn>
2023-08-01 11:17:22 +0800

    Delete 'start' module & update Gradle version to 8.1.0

--- basic/src/main/java/com/compose/basic/BasicActivity.kt
+++ basic/src/main/java/com/compose/basic/BasicActivity.kt
@@ -12,8 +12,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,7 +42,7 @@ class BasicActivity : ComponentActivity() {
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     MaterialTheme {
-        Surface(color = Color.Yellow) {
+        Surface(color = Color.White) {
             content()
         }
     }
